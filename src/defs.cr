96a2dce3d3d238d3539f3c17ac3fbc757456d218
tomerun <tomerunmail@gmail.com>
2020-07-18 07:24:31 +0900

    implemented foundation

---
+++ src/defs.cr
@@ -0,0 +1,283 @@
+require "big"
+
+abstract class Node
+  abstract def name
+end
+
+abstract class Arity0 < Node
+  def param_count
+    return 0
+  end
+
+  def param_full?
+    return true
+  end
+
+  def to_s(io)
+    io << self.name
+  end
+end
+
+abstract class Arity1 < Node
+  getter :x0
+
+  def initialize(@x0 : Node | Nil = nil)
+  end
+
+  def param_count
+    return @x0.nil? ? 0 : 1
+  end
+
+  def param_full?
+    return !@x0.nil?
+  end
+
+  def to_s(io)
+    if !@x0
+      io << self.name
+    else
+      io << "ap #{self.name} " << @x0
+    end
+  end
+end
+
+abstract class Arity2 < Node
+  getter :x0, :x1
+
+  def initialize(@x0 : Node | Nil = nil, @x1 : Node | Nil = nil)
+  end
+
+  def param_count
+    return @x0.nil? ? 0 : @x1.nil? ? 1 : 2
+  end
+
+  def param_full?
+    return !@x1.nil?
+  end
+
+  def to_s(io)
+    if !@x0
+      io << self.name
+    elsif !@x1
+      io << "ap #{self.name} " << @x0
+    else
+      io << "ap ap #{self.name} " << @x0 << " " << @x1
+    end
+  end
+end
+
+abstract class Arity3 < Node
+  getter :x0, :x1, :x2
+
+  def initialize(@x0 : Node | Nil = nil, @x1 : Node | Nil = nil, @x2 : Node | Nil = nil)
+  end
+
+  def param_count
+    return @x0.nil? ? 0 : @x1.nil? ? 1 : @x2.nil? ? 2 : 3
+  end
+
+  def param_full?
+    return !@x2.nil?
+  end
+
+  def to_s(io)
+    if !@x0
+      io << self.name
+    elsif !@x1
+      io << "ap #{self.name} " << @x0
+    elsif !@x2
+      io << "ap ap #{self.name} " << @x0 << " " << @x1
+    else
+      io << "ap ap ap #{self.name} " << @x0 << " " << @x1 << " " << @x2
+    end
+  end
+end
+
+class IntAtom < Arity0
+  getter :v
+
+  def initialize(@v : BigInt)
+  end
+
+  def initialize(v : Int)
+    @v = BigInt.new(v)
+  end
+
+  def to_s(io)
+    io << @v
+  end
+
+  def name
+    @v.to_s
+  end
+end
+
+class Inc < Arity1
+  def name
+    "inc"
+  end
+end
+
+class Dec < Arity1
+  def name
+    "dec"
+  end
+end
+
+class Add < Arity2
+  def name
+    "add"
+  end
+end
+
+class Var < Arity0
+  getter :n
+
+  def initialize(@n : Int32)
+  end
+
+  def name
+    ":#{@n}"
+  end
+end
+
+class Mul < Arity2
+  def name
+    "mul"
+  end
+end
+
+class Div < Arity2
+  def name
+    "div"
+  end
+end
+
+class Eq < Arity2
+  def name
+    "eq"
+  end
+end
+
+class LessThan < Arity2
+  def name
+    "lt"
+  end
+end
+
+class Neg < Arity1
+  def name
+    "neg"
+  end
+end
+
+class Ap < Arity2
+  def name
+    "ap"
+  end
+
+  def to_s(io)
+    # special case
+    io << "ap " << @x0 << " " << @x1
+  end
+end
+
+class Scomb < Arity3
+  def name
+    "s"
+  end
+end
+
+class Ccomb < Arity3
+  def name
+    "c"
+  end
+end
+
+class Bcomb < Arity3
+  def name
+    "b"
+  end
+end
+
+class True < Arity2
+  def name
+    "t"
+  end
+end
+
+class False < Arity2
+  def name
+    "f"
+  end
+end
+
+class Pwr2 < Arity1
+  def name
+    "pwr2"
+  end
+end
+
+class Icomb < Arity1
+  def name
+    "i"
+  end
+end
+
+class Cons < Arity3
+  def car
+    @x0
+  end
+
+  def cdr
+    @x1
+  end
+
+  def name
+    "cons"
+  end
+end
+
+class Car < Arity1
+  def name
+    "car"
+  end
+end
+
+class Cdr < Arity1
+  def name
+    "cdr"
+  end
+end
+
+class NilAtom < Arity1
+  def name
+    "nil"
+  end
+end
+
+class IsNil < Arity1
+  def name
+    "isnil"
+  end
+end
+
+class IfZero < Arity3
+  def name
+    "if0"
+  end
+end
+
+class Assign
+  getter :var, :node
+
+  def initialize(@var : Var, @node : Node)
+  end
+
+  def to_s(io)
+    io << @var << " = " << @node
+  end
+end
+
+class TypeMismatchError < Exception
+end
---
+++ src/modem.cr
@@ -0,0 +1,93 @@
+require "./defs.cr"
+
+def mod(v : NilAtom, io)
+  io << "00"
+end
+
+def mod(v : IntAtom, io)
+  i = v.v
+  if i < 0
+    io << "10"
+    i = i.abs
+  else
+    io << "01"
+  end
+  base2 = i.to_s(2)
+  len = i == 0 ? 0 : (base2.size + 3) // 4
+  io << "1" * len << "0"
+  if len > 0
+    io << "0" * (len * 4 - base2.size) << base2
+  end
+end
+
+def mod(v : Cons, io)
+  io << "11"
+  case car = v.car
+  when Cons
+    mod(car, io)
+  when IntAtom
+    mod(car, io)
+  when NilAtom
+    mod(car, io)
+  else
+    raise TypeMismatchError.new(car.to_s)
+  end
+  case cdr = v.cdr
+  when Cons
+    mod(cdr, io)
+  when IntAtom
+    mod(cdr, io)
+  when NilAtom
+    mod(cdr, io)
+  else
+    raise TypeMismatchError.new(cdr.to_s)
+  end
+end
+
+def mod(node : Node) : String
+  return String.build do |io|
+    case node
+    when Cons
+      mod(node, io)
+    when IntAtom
+      mod(node, io)
+    when NilAtom
+      mod(node, io)
+    else
+      raise TypeMismatchError.new(node.to_s)
+    end
+  end
+end
+
+def demod(str : String, pos : Int32) : Tuple(Node, Int32)
+  if str[pos] == '1' && str[pos + 1] == '1'
+    car, pos = demod(str, pos + 2)
+    cdr, pos = demod(str, pos)
+    return {Cons.new(car, cdr), pos}
+  elsif str[pos] == '0' && str[pos + 1] == '0'
+    return {NilAtom.new, pos + 2}
+  else
+    sign = str[pos] == '0' ? 1 : -1
+    pos += 2
+    len = 0
+    while str[pos] == '1'
+      len += 1
+      pos += 1
+    end
+    pos += 1
+    if len == 0
+      return {IntAtom.new(BigInt.new(0)), pos}
+    else
+      last = pos + 4 * len
+      return {IntAtom.new(BigInt.new(str[pos...last], base: 2) * sign), last}
+    end
+  end
+end
+
+def demod(str : String) : Node
+  return demod(str, 0)[0]
+end
+
+while true
+  puts demod(read_line)
+end
